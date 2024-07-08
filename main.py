8ef90975dc321ed977bded39432e2cd3e94476e4
listenzcc <listenzcc@mail.bnu.edu.cn>
2024-07-08 20:25:27 +0800

    Init commit

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+File: main.py
+Author: Chuncheng Zhang
+Date: 2024-07-08
+Copyright & Email: chuncheng.zhang@ia.ac.cn
+
+Purpose:
+    Main entrance
+
+Functions:
+    1. Requirements and constants
+    2. Function and class
+    3. Play ground
+    4. Pending
+    5. Pending
+"""
+
+
+# %% ---- 2024-07-08 ------------------------
+# Requirements and constants
+
+
+# %% ---- 2024-07-08 ------------------------
+# Function and class
+
+
+# %% ---- 2024-07-08 ------------------------
+# Play ground
+
+
+# %% ---- 2024-07-08 ------------------------
+# Pending
+
+
+# %% ---- 2024-07-08 ------------------------
+# Pending
